22d226a14c5a289b0159b3155214ee5b48ae19a5
merveyasar <merve.yasar.ce@gmail.com>
2026-02-26 23:09:33 +0300

    Fix: Add WhiteNoise for static files

--- emk_web/settings.py
+++ emk_web/settings.py
@@ -53,13 +53,30 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'emk_web.urls'
 
+# TEMPLATES = [
+#     {
+#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#         'DIRS': [],
+#         'APP_DIRS': True,
+#         'OPTIONS': {
+#             'context_processors': [
+#                 'django.template.context_processors.request',
+#                 'django.contrib.auth.context_processors.auth',
+#                 'django.contrib.messages.context_processors.messages',
+#             ],
+#         },
+#     },
+# ]
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], # Burayı bu hale getir
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
